349d9b9b937ea3c22601e34e0df73e92c6b27eac
John Sparwasser <johnspar1@gmail.com>
2017-03-02 16:13:32 -0500

    Autoloader fixes

--- autoloader.php
+++ autoloader.php
@@ -2,10 +2,11 @@
 
 function registerSubscribeProSDK($className) {
 
-    $className = str_replace("\\", "/", $className);
-    $file = __DIR__ . "/src/" . $className . '.php';
+    $className = str_replace("\\", '/', $className);
+    $file = __DIR__ . '/src/' . $className . '.php';
     include_once($file);
 
 }
 
 spl_autoload_register('registerSubscribeProSDK');
+include_once(__DIR__ . '/vendor/autoload.php');
