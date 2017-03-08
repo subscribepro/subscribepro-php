e7cf8c9369c8f8bace2181144eaac95e2b425dc6
John Sparwasser <johnspar1@gmail.com>
2017-03-08 12:30:56 -0500

    Added the notification field to the creation attributes

--- src/SubscribePro/Service/Subscription/Subscription.php
+++ src/SubscribePro/Service/Subscription/Subscription.php
@@ -29,7 +29,7 @@ class Subscription extends DataObject implements SubscriptionInterface
         self::INTERVAL => true,
         self::NEXT_ORDER_DATE => true,
         self::FIRST_ORDER_ALREADY_CREATED => false,
-        self::SEND_CUSTOMER_NOTIFICATION_EMAIL => false,
+        self::SEND_CUSTOMER_NOTIFICATION_EMAIL => true,
         self::EXPIRATION_DATE => false,
         self::COUPON_CODE => false,
         self::MAGENTO_STORE_CODE => false,
