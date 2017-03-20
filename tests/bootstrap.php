4210265261e9a3bfa34bd44ca5c6dd9a92cb6f05
John Sparwasser <johnspar1@gmail.com>
2017-03-20 17:16:25 -0400

    PHPUnit backwards compatibility

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,9 @@ error_reporting(-1);
 date_default_timezone_set('UTC');
 
 // PHP Unit backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') &&
-    class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+if (class_exists('\PHPUnit\Framework\TestCase') &&
+    !class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
 
 // Include the composer autoloader
